191803c8c150c7915096ab0107696fcf83c46a93
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-01 18:07:48 -0400

    sunday#2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,23 +6,24 @@
         , "astronomical unit", "astronomy", "axial tilt", "azimuth"]
     var haveWord = false;
     var answerArray = [];
-
+    var wins = 0;
 
     // Game start button calls generate word function
         document.getElementsByClassName("btn-start-game")[0].onclick = function getWord() {    
             document.getElementsByClassName("word-fillout")[0].textContent = "";
-            // document.getElementsByClassName("guesses").empty();
-            // document.getElementsByClassName("letters-guessed").empty();
             var guesses = 0;
             var guessLimit = 10;
             var word = words[Math.floor(Math.random() * words.length)];
             word_arr = word.split(""); //array of new word
             haveWord = true;
+            var remainingLetters = word.length;
 
+            // builds answerArray with underscores
             for (var i = 0; i < word.length;i++){
                 answerArray[i] = "_";
-            };
-            document.getElementsByClassName("word-fillout")[0].textContent = answerArray;
+                document.getElementsByClassName("word-fillout")[0].textContent = "_";
+            };             
+            
 
         // checks user input against word that is loaded
         document.onkeyup = function () {
@@ -30,26 +31,31 @@
                 var guess = event.key
                 var _guesses = [];
                 var x = word.indexOf(event.key);
+
                 if (x == -1) {
                     document.getElementsByClassName("guesses")[0].textContent = (++guesses);
                     document.getElementsByClassName("letters-guessed")[0].append(event.key);
                     document.getElementsByClassName("letters-guessed")[0].append(" ");
                     _guesses.push(String.fromCharCode(event.key));
-                    console.log(_guesses);
                     }
                     // user guesses correct letter, 
                     // replace letter in answerarray with guess
                         else {
-                            answerArray[x] = guess;
-                            document.getElementsByClassName("word-fillout")[0].textContent = answerArray;
-                            // console.log(answerArray.join(word_arr);
-                            
-                            alert("you guessed a correct letter!");
+                            for (var y = 0 ; y < word.length ; y++){
+                                if(word[y] == guess){
+                                    answerArray[y] = guess;
+                                    remainingLetters--
+                                }
+                            }
                         };
-            // document.getElementsByClassName("word-answer")[0].textContent = (x);
-            
+                    document.getElementsByClassName("word-fillout")[0].textContent = answerArray;
+                if (remainingLetters == 0){
+                    alert("you win!");
+                    document.getElementsByClassName("wins")[0].textContent = ++wins;
+                }
             };
         };
+        // display answer for debugging
         document.getElementsByClassName("word-answer")[0].textContent = (word);
     };
 
