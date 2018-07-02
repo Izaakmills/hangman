8a8df946b5bad05e564e7deb12469dab85f9db32
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-01 21:12:42 -0400

    finally#2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,41 +2,42 @@
 // 1. have user hit a button to create a row of underscores that equals the number of letters in word
 // 2. 
 
-var words = ["dog","cat","mouse","bird","bat","horse","monkey","spider"]
+var words = ["dog", "cat", "mouse", "bird", "bat", "horse", "monkey", "spider"]
 var haveWord = false;
 var answerArray = [];
 var wins = 0;
-var losses =0;
+var losses = 0;
+var guesses = 9;
 
-// Game start button calls generate word function
-document.getElementsByClassName("btn-start-game")[0].onclick = function getWord() {
-    var guesses = 8;
+
+// activates game
+document.getElementsByClassName("btn-start")[0].onclick = function getWord() {
     var word = words[Math.floor(Math.random() * words.length)];
     word_arr = word.split(""); //array of new word
     haveWord = true;
     var remainingLetters = word.length;
+    console.log(word);
 
     // builds answerArray with underscores
     for (var i = 0; i < word.length; i++) {
         answerArray[i] = "_";
         document.getElementsByClassName("word-fillout")[i] = "_";
     };
-    document.getElementsByClassName("word-fillout")[0].textContent = answerArray;
-
-// checks user input against word that is loaded
-document.onkeyup = function () {
-    if (haveWord) {
+    document.getElementsByClassName("word-fillout")[0].textContent = answerArray.join(" ");
+    // Game start button calls generate word function
+    document.onkeyup = function () {
         var guess = event.key
-        var _guesses = 8;
         var x = word.indexOf(event.key);
 
         document.getElementsByClassName("letters-guessed")[0].append(event.key);
         document.getElementsByClassName("letters-guessed")[0].append(" ");
+
+        // user guesses wrong letter
         if (x == -1) {
             document.getElementsByClassName("guesses")[0].textContent = (--guesses);
         }
         // user guesses correct letter, 
-        // replace letter in answerarray with guess
+        // replace letter in answerArray with guess
         else {
             for (var y = 0; y < word.length; y++) {
                 if (word[y] == guess) {
@@ -45,8 +46,7 @@ document.onkeyup = function () {
                 }
             }
         };
-        document.getElementsByClassName("word-fillout")[0].textContent = answerArray;
-       
+        document.getElementsByClassName("word-fillout")[0].textContent = answerArray.join(" ");
 
         if (guesses == 0) {
             alert("you lose!");
@@ -55,8 +55,10 @@ document.onkeyup = function () {
             document.getElementsByClassName("letters-guessed")[0].textContent = " ";
             document.getElementsByClassName("guesses")[0].textContent = " ";
             answerArray = [];
+            guesses = 8;
+            keyLogger = [];
+            getWord()
         }
-
 
         if (remainingLetters == 0) {
             alert("you win!");
@@ -64,10 +66,15 @@ document.onkeyup = function () {
             document.getElementsByClassName("word-fillout")[0].textContent = "";
             document.getElementsByClassName("letters-guessed")[0].textContent = " ";
             document.getElementsByClassName("guesses")[0].textContent = " ";
+            answerArray = [];
+            guesses = 8;
+            keyLogger = [];
+            getWord()
+
         }
-    };
+    }
 };
-};
+
     // display answer for debugging
     // document.getElementsByClassName("word-answer")[0].textContent = (word);
 
