8c5d4c90b079ca6b8d935d4b81080877e7eae83c
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-29 01:06:56 -0400

    fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@
 var words = ["dog", "cat", "mouse", "bird", "bat", "horse", "monkey", "spider"]
 var haveWord = false;
 var answerArray = [];
+var guessArray = [];
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -12,6 +13,8 @@ var guesses = 9;
 
 // activates game
 document.getElementsByClassName("btn-start")[0].onclick = function getWord() {
+    this.style.display = "hidden"
+
     var word = words[Math.floor(Math.random() * words.length)];
     word_arr = word.split(""); //array of new word
     haveWord = true;
@@ -23,29 +26,35 @@ document.getElementsByClassName("btn-start")[0].onclick = function getWord() {
         answerArray[i] = "_";
         document.getElementsByClassName("word-fillout")[i] = "_";
     };
+
     document.getElementsByClassName("word-fillout")[0].textContent = answerArray.join(" ");
     // Game start button calls generate word function
     document.onkeyup = function () {
         var guess = event.key
         var x = word.indexOf(event.key);
+        console.log(guess)
 
-        document.getElementsByClassName("letters-guessed")[0].append(event.key);
-        document.getElementsByClassName("letters-guessed")[0].append(" ");
+        // if letter has not been guesses proceed, if it has ignore it
+        if (guessArray.indexOf(guess) === -1){
+            guessArray.push(guess)
+            document.getElementsByClassName("letters-guessed")[0].append(guess);
+            document.getElementsByClassName("letters-guessed")[0].append(" ");
 
-        // user guesses wrong letter
-        if (x == -1) {
-            document.getElementsByClassName("guesses")[0].textContent = (--guesses);
-        }
-        // user guesses correct letter, 
-        // replace letter in answerArray with guess
-        else {
-            for (var y = 0; y < word.length; y++) {
-                if (word[y] == guess) {
-                    answerArray[y] = guess;
-                    remainingLetters--;
-                }
+            // user guesses wrong letter
+            if (x == -1) {
+                document.getElementsByClassName("guesses")[0].textContent = (--guesses);
             }
-        };
+            // user guesses correct letter, 
+            // replace letter in answerArray with guess
+            else {
+                for (var y = 0; y < word.length; y++) {
+                    if (word[y] == guess) {
+                        answerArray[y] = guess;
+                        remainingLetters--;
+                    }
+                }
+            };
+        } 
         document.getElementsByClassName("word-fillout")[0].textContent = answerArray.join(" ");
 
         if (guesses == 0) {
@@ -56,7 +65,9 @@ document.getElementsByClassName("btn-start")[0].onclick = function getWord() {
             document.getElementsByClassName("guesses")[0].textContent = " ";
             answerArray = [];
             guesses = 8;
+            guessArray = [];
             keyLogger = [];
+            document.getElementsByClassName("btn-start")[0].display = "visible"
             getWord()
         }
 
@@ -68,7 +79,9 @@ document.getElementsByClassName("btn-start")[0].onclick = function getWord() {
             document.getElementsByClassName("guesses")[0].textContent = " ";
             answerArray = [];
             guesses = 8;
+            guessArray = [];
             keyLogger = [];
+            document.getElementsByClassName("btn-start")[0].display = "visible"
             getWord()
 
         }
